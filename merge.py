3a84c951b18d68d7cbe94c2b2283f2379a94824f
lixx11 <lxx2011011580@gmail.com>
2017-06-06 11:05:08 +0800

    calculate wavelength for each event

--- merge.py
+++ merge.py
@@ -2,7 +2,7 @@
 
 """
 Usage:
-  merge.py <peak_list_dir> <spind_indexing_dir> <point_group> [options]
+  merge.py <peak_list_dir> <spind_indexing_dir> <point_group> <det_dist>[options]
 
 Options:
   -h --help                         Show this screen.
@@ -11,20 +11,23 @@ Options:
   --odd-only                        Merge odd events only.
   --even-only                       Merge even events only.
   --pixel-size=pixel_size           Pixel size in meters [default: 110E-6].
-  --wave-length=wave_length         Wave length in meters[default: 1.280e-10].
-  --det-dist=det_dist               Detector distance in meters [default: 92.40E-3]. 
   --min-match-rate=min_match_rate   Min match rate accepted while merging [default: 0.5].
   --eval-tol=eval_tol               HKL tolerence between observed peaks and predicted spots [default: 0.25].
   --hkl-diag                        Output eHKL for debug.
 """
 
 import numpy as np 
+import h5py
 import math
 from docopt import docopt
 from glob import glob
 import os
 import sys
 from tqdm import tqdm
+
+
+h_ = 4.135667662E-15  # Planck constant in eV*s
+c_ = 2.99792458E8  # light speed in m/sec
 
 
 def get_hkl(q, A=None, A_inv=None):
@@ -113,7 +116,7 @@ def write2hkl(ref_dict, pointgroup, hkl_file='merge.hkl'):
     f.write('CrystFEL reflection list version 2.0\n')
     f.write('Symmetry: %s\n' % point_group)
     f.write('   h    k    l          I    phase   sigma(I)   nmeas\n')
-    for i in tqdm(range(len(sorted_refls))):
+    for i in range(len(sorted_refls)):
         refl = sorted_refls[i][1]
         intensity = np.mean(refl.intensity_list)
         redundancy = len(refl.intensity_list)
@@ -133,8 +136,8 @@ if __name__ == '__main__':
     peak_dir = argv['<peak_list_dir>']  # peak lists extracted from cxi
     point_group = argv['<point_group>']
     pixel_size = float(argv['--pixel-size'])
-    det_dist = float(argv['--det-dist'])
-    wave_length = float(argv['--wave-length'])
+    det_dist = float(argv['<det_dist>'])
+    # wave_length = float(argv['<wave_length>'])
     min_match_rate = float(argv['--min-match-rate'])
     eval_tol = float(argv['--eval-tol'])
     output_file = argv['--output']
@@ -142,87 +145,98 @@ if __name__ == '__main__':
     hkl_diag = argv['--hkl-diag']
 
     nb_merge = 0  # number of merged pattern
+    nb_event = 0  # number of event processed
+    nb_meas = 0  # number of measurements merged
     eHKLs = []
-    peak_files = glob('%s/*.txt' % peak_dir)
+    peak_files = glob('%s/*.h5' % peak_dir)
     reflection_dict = {}
+    spind_dict = {}
 
-    print('Processing peak files')
-    for i in tqdm(range(len(peak_files))):
-        if argv['--odd-only']:
-            if i % 2 == 0:
-                continue
-        elif argv['--even-only']:
-            if i % 2 == 1:
-                continue
+    # loading all the SPIND indexing files
+    spind_files = glob('%s/*spind.txt' % spind_dir)
+    for spind_file in spind_files:
+        basename = os.path.basename(spind_file)
+        spind_dict[basename[:-10]] = np.loadtxt(spind_file)
+
+    for i in range(len(peak_files)):
         peak_file = peak_files[i]
+        print('Processing %s' % peak_file)
+        peak_h5 = h5py.File(peak_file, 'r')
+        for key in peak_h5.keys():
+            peak_data = peak_h5[key].value
+            peak_coord_pixel = peak_data[:,:2]
+            peak_coord = peak_coord_pixel * pixel_size
+            intensity = peak_data[:,2]
+            photon_energy = peak_data[0,4]  # in eV
+            wave_length = h_ * c_ / photon_energy
+            qs = det2fourier(peak_coord, wave_length, det_dist) 
 
-        peak_data = np.loadtxt(peak_file)
-        peak_coord_pixel = peak_data[:,:2]
-        peak_coord = peak_coord_pixel * pixel_size
-        intensity = peak_data[:,2]
-        qs = det2fourier(peak_coord, wave_length, det_dist)
-
-        peak_basename = os.path.basename(peak_file)
-        event_id = int(peak_basename[-8:-4])
-        spind_file = os.path.join(spind_dir, '%s-spind.txt' %
-            peak_basename[:-10])
-        if not os.path.exists(spind_file):
-            continue
-        spind_data = np.loadtxt(spind_file)
-        event_ids = spind_data[:,0].astype(np.int)
-        row = np.where(event_ids == event_id)[0]
-        if row.size == 0:
-            if verbose:
-                print('event %s not found in %s' % 
-                    (event_id, os.path.basename(spind_file)))
-            continue
-        else:
-            assert row.size == 1
-            row = row[0]
-            match_rate = spind_data[row][1]
-            if match_rate < min_match_rate:
+            event_id = int(key[-4:])
+            spind_key = key[:-6]
+            if not spind_dict.has_key(spind_key):
+                continue
+            spind_data = spind_dict[spind_key]
+            event_ids = spind_data[:,0].astype(np.int)
+            row = np.where(event_ids == event_id)[0]
+            if row.size == 0:
                 if verbose:
-                    print('event %s failed in match rate test in %s' %
+                    print('event %s not found in %s' % 
                         (event_id, os.path.basename(spind_file)))
                 continue
-            # pass match rate test
-            nb_merge += 1
-            A = np.ones((3,3))
-            A[:,0] = spind_data[row][4:7]
-            A[:,1] = spind_data[row][7:10]
-            A[:,2] = spind_data[row][10:13]
-            HKL = get_hkl(qs, A=A)  # decimal hkls
-            rHKL = np.round(HKL)
-            eHKL = np.abs(HKL - rHKL)
-            eHKLs += eHKL.tolist()
-            pair_ids = np.where(eHKL.max(axis=1) < eval_tol)[0]
-            nb_pair = len(pair_ids)
-            if float(nb_pair) / float(peak_data.shape[0]) < min_match_rate:
-                if verbose:
-                    print('Warning: match rate recalculated failed test!!!')
-            for j, pair_id in enumerate(pair_ids):
-                hkl = rHKL[pair_id].astype(np.int)
-                if point_group == 'mmm':
-                    h,k,l = abs(hkl[0]), abs(hkl[1]), abs(hkl[2])
-                elif point_group == '2':
-                    h = abs(hkl[0])
-                    k = hkl[1]
-                    if hkl[0] >= 0:
-                        l = hkl[2]
+            else:
+                assert row.size == 1
+                row = row[0]
+                match_rate = spind_data[row][1]
+                if match_rate < min_match_rate:
+                    if verbose:
+                        print('event %s failed in match rate test in %s' %
+                            (event_id, os.path.basename(spind_file)))
+                    continue
+                # pass match rate test
+                nb_event += 1
+                if argv['--odd-only'] and (nb_event % 2) == 0:
+                    continue
+                elif argv['--even-only'] and (nb_event % 2) == 1:
+                    continue
+                nb_merge += 1
+                A = np.ones((3,3))
+                A[:,0] = spind_data[row][4:7]
+                A[:,1] = spind_data[row][7:10]
+                A[:,2] = spind_data[row][10:13]
+                HKL = get_hkl(qs, A=A)  # decimal hkls
+                rHKL = np.round(HKL)
+                eHKL = np.abs(HKL - rHKL)
+                eHKLs += eHKL.tolist()
+                pair_ids = np.where(eHKL.max(axis=1) < eval_tol)[0]
+                nb_pair = len(pair_ids)
+                if float(nb_pair) / float(peak_data.shape[0]) < min_match_rate:
+                    if verbose:
+                        print('Warning: match rate recalculated failed test!!!')
+                for j, pair_id in enumerate(pair_ids):
+                    hkl = rHKL[pair_id].astype(np.int)
+                    if point_group == 'mmm':
+                        h,k,l = abs(hkl[0]), abs(hkl[1]), abs(hkl[2])
+                    elif point_group == '2':
+                        h = abs(hkl[0])
+                        k = hkl[1]
+                        if hkl[0] >= 0:
+                            l = hkl[2]
+                        else:
+                            l = -hkl[2]
                     else:
-                        l = -hkl[2]
-                else:
-                    print('point group %s not implemented' % point_group)
-                    sys.exit()
-                int_ = hkl2int_(h, k, l)
-                if reflection_dict.has_key(int_):
-                    reflection_dict[int_].add_measure(intensity[pair_id])
-                else:
-                    reflection = Reflection(h, k, l)
-                    reflection.add_measure(intensity[pair_id])
-                    reflection_dict[reflection.int_] = reflection
+                        print('point group %s not implemented' % point_group)
+                        sys.exit()
+                    int_ = hkl2int_(h, k, l)
+                    nb_meas += 1
+                    if reflection_dict.has_key(int_):
+                        reflection_dict[int_].add_measure(intensity[pair_id])
+                    else:
+                        reflection = Reflection(h, k, l)
+                        reflection.add_measure(intensity[pair_id])
+                        reflection_dict[reflection.int_] = reflection
 
-    print('%d events merged.' % nb_merge)
+    print('%d events processed, %d event merged with %d measurements.' % 
+        (nb_event, nb_merge, nb_meas))
 
     # write to hkl file
     print('write hkl to %s' % output_file)
